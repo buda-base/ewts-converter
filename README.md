dd47d60c47e495b111fb6bcb2942010ab0d894c4
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-04-14 11:03:00 +0200

    move doc to README

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# ewts-converter
-Java Tibetan EWTS &lt;-> Unicode converter
+# Ewts Converter
+
+This Java package implements the conversion between Unicode Tibetan text, and [Extended Wylie transliteration (EWTS)](http://www.thlib.org/reference/transliteration/#essay=/thl/ewts/).
+
+It is based on the equivalent Perl module, [Lingua::BO::Converter](http://www.digitaltibetan.org/cgi-bin/wylie.pl).
+
+## Installation
+
+Using maven:
+
+```xml
+	<dependency>
+      <groupId>io.bdrc.ewtsconverter</groupId>
+      <artifactId>ewts-converter</artifactId>
+      <version>1.1.0</version>
+    </dependency>
+```
+
+### Use
+
+```java
+import io.bdrc.ewtsconverter.EwtsConverter;
+
+EwtsConverter wl = new EwtsConverter();
+System.out.println(wl.toUnicode("sems can thams cad"));
+System.out.println(wl.toWylie("\u0f66\u0f44\u0f66\u0f0b\u0f62\u0f92\u0fb1\u0f66\u000a"));
+```
+
+### Options
+
+You can pass some options to the constructor:
+
+```java
+EwtsConverter(boolean check, boolean check_strict, boolean print_warnings, boolean fix_spacing)
+```
+
+- `check`: generate warnings for illegal consonant sequences; default is `true`.
+- `check_strict`: stricter checking, examine the whole stack; default is `true`.
+- `print_warnings`: print generated warnings to `System.out`; default is `false`.
+- `fix_spacing`: remove spaces after newlines, collapse multiple tseks into one, fix case, etc; default is `true`.
+
+### API
+
+##### String toUnicode(String wylie_string)
+
+Converts from Converter (EWTS) to Unicode.
+
+##### String toUnicode(String wylie_string, ArrayList<String> warns)
+
+Converts from Converter (EWTS) to Unicode; puts the generated warnings in the list.
+
+##### String toWylie(String unicode_string)
+
+Converts from Unicode to Converter. Anything that is not Tibetan Unicode is converted to EWTS comment blocks [between brackets].
+
+##### String toWylie(String unicode_string, ArrayList<String> warns, boolean escape)
+
+Converts from Unicode to Converter.   Puts the generated warnings in the list. If escape is false, anything that is not Tibetan Unicode is just passed through as it is.
+
+### Performance and Concurrency
+
+This code should perform quite decently.  When converting from Converter to
+Unicode, the entire string is split into tokens, which are themselves
+strings.  If this takes too much memory, consider converting your text in
+smaller chunks.  With today's computers, it should not be a problem to
+convert several megabytes of tibetan text in one call.  Otherwise, it could
+be worthwhile to tokenize the input on the fly, rather than all at once.
+
+This class is entirely thread-safe.  In a multi-threaded environment,
+multiple threads can share the same instance without any problems.
+
+### License
+
+For simplicity reasons, we distribute our modifications only under the [Apache 2.0 Licens](LICENSE), but the original version had this statement:
+
+```txt
+This library is Free Software.  You can redistribute it or modify it, under
+the terms of, at your choice, the GNU General Public License (version 2 or
+higher), the GNU Lesser General Public License (version 2 or higher), the
+Mozilla Public License (any version) or the Apache License version 2 or
+higher.
+
+Please contact the author if you wish to use it under some terms not covered
+here.
+```
--- src/main/java/io/bdrc/ewtsconverter/EwtsConverter.java
+++ src/main/java/io/bdrc/ewtsconverter/EwtsConverter.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2014 Tibetan Buddhist Resource Center (TBRC)
+ * Copyright (C) 2010 Roger Espel Llima
+ * Copyright (c) 2011-2017 Buddhist Digital Resource Center (BDRC)
  * 
  * If this file is a derivation of another work the license header will appear below; 
  * otherwise, this work is licensed under the Apache License, Version 2.0 
@@ -16,109 +17,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-/* This Java package implements the conversion between Unicode Tibetan text, and
-   Converter (EWTS) transliteration.
-
-   It is based on the equivalent Perl module, Lingua::BO::Converter, found at 
-   http://www.digitaltibetan.org/cgi-bin/wylie.pl
-
-   The Extended Converter Transliteration System is documented at:
-   http://www.thlib.org/reference/transliteration/#essay=/thl/ewts/
-
-   OVERVIEW
-   ========
-
-   This code is a library; it is meant to be embedded in other software.
-
-      import org.rigpa.wylie.Wylie;
-         ...
-      Converter wl = new Converter();
-      System.out.println(wl.toUnicode("sems can thams cad"));
-      System.out.println(wl.toWylie("\u0f66\u0f44\u0f66\u0f0b\u0f62\u0f92\u0fb1\u0f66\u000a"));
-
-   From the command-line, you can use the provided "Test" and "TestToWylie"
-   programs, which requests lines in Converter from the user and prints the unicode to
-   stdout, and vice versa.  Run them with:
-      java Test
-      java TestToWylie
-
-   To run the test suite, run this command:
-      javac -g RunTests.java && java RunTests < test.txt
-
-
-   CONSTRUCTOR
-   ===========
-
-   Two constructors are available:
-
-       Converter wl = new Converter();		// uses all the default options
-   and
-       Converter wl = new Converter(check, check_strict, print_warnings, fix_spacing);
-
-   The meanings of the parameters are:
-   - check (boolean)          : generate warnings for illegal consonant sequences; default is true.
-   - check_strict (boolean)   : stricter checking, examine the whole stack; default is true.
-   - print_warnings (boolean) : print generated warnings to STDOUT; default is false.
-   - fix_spacing (boolean)    : remove spaces after newlines, collapse multiple tseks into one, etc;
-				default is true.
-
-   PUBLIC METHODS
-   ==============
-
-   String toUnicode(String wylie_string);
-      Converts from Converter (EWTS) to Unicode.
-
-   String toUnicode(String wylie_string, ArrayList<String> warns);
-      Converts from Converter (EWTS) to Unicode; puts the generated warnings in the list.
-
-   String toWylie(String unicode_string);
-      Converts from Unicode to Converter. 
-      Anything that is not Tibetan Unicode is converted to EWTS comment blocks [between brackets].
-
-   String toWylie(String unicode_string, ArrayList<String> warns, boolean escape);
-      Converts from Unicode to Converter.   Puts the generated warnings in the list.
-      If escape is false, anything that is not Tibetan Unicode is just passed through as it is.
-
-
-   PERFORMANCE AND CONCURRENCY
-   ===========================
-
-   This code should perform quite decently.  When converting from Converter to
-   Unicode, the entire string is split into tokens, which are themselves
-   strings.  If this takes too much memory, consider converting your text in
-   smaller chunks.  With today's computers, it should not be a problem to
-   convert several megabytes of tibetan text in one call.  Otherwise, it could
-   be worthwhile to tokenize the input on the fly, rather than all at once.
-
-   This class is entirely thread-safe.  In a multi-threaded environment,
-   multiple threads can share the same instance without any problems.
-
-
-   AUTHOR AND LICENSE
-   ==================
-
-   Copyright (C) 2010 Roger Espel Llima
-
-   This library is Free Software.  You can redistribute it or modify it, under
-   the terms of, at your choice, the GNU General Public License (version 2 or
-   higher), the GNU Lesser General Public License (version 2 or higher), the
-   Mozilla Public License (any version) or the Apache License version 2 or
-   higher.
-
-   Please contact the author if you wish to use it under some terms not covered
-   here.
-
-   Copyright 2011 TBRC
-   
-   This library is Free Software.  You can redistribute it or modify it, under
-   the terms of, at your choice, the GNU General Public License (version 2 or
-   higher), the GNU Lesser General Public License (version 2 or higher), the
-   Mozilla Public License (any version) or the Apache License version 2 or
-   higher.
-  
-   Please contact support@tbrc.org if you wish to use it under some terms not covered
-   here.
-*/
 
 package io.bdrc.ewtsconverter;
 
